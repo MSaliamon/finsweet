ff29ab6a3aef2fe41d93175e3435a792b60dcd17
MSaliamon <maksimsalamon181@gmail.com>
2025-05-14 10:46:52 +0300

    section3-done

---
+++ src/components/Main/Section3/Section3.tsx
@@ -0,0 +1,19 @@
+import './Section3.scss'
+function Section3() {
+  return (
+    <section className="section3">
+        <div className="section3-container1">
+            <p className="section3-container1__sub-title">ABOUT US</p>
+            <h2 className="section3-container1__title">We are a community of content writers who share their learnings</h2>
+            <p className="section3-container1__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <a href="#" className="section3-container1__link">Read More</a>
+        </div>
+        <div className="section3-container2">
+            <p className="section3-container2__sub-title">Our mision</p>
+            <h2 className="section3-container2__title">Creating valuable content for creatives all around the world</h2>
+            <p className="section3-container2__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </section>
+  )
+}
+export default Section3
