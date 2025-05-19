a5bdc432e7cc4ef6b7845a490e74a4cd259a9fac
MSaliamon <maksimsalamon181@gmail.com>
2025-05-19 09:57:39 +0300

    section6-done

---
+++ src/components/Main/Section6/Section6.tsx
@@ -0,0 +1,15 @@
+import './Section6.scss'
+
+function Section6() {
+  return (
+    <section className="section6">
+        <div className="section6-textbox">
+            <h2 className="section6-textbox__title">Join our team to be a part of our story</h2>
+            <p className="section6-textbox__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+        <button className="section6__btn button">Join Now</button>
+    </section>
+  )
+}
+
+export default Section6
